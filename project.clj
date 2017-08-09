b4d38d333adf104d43aae92f3cd959dcf30795c4
Henri Kauppinen <kauppinen.henri@gmail.com>
2017-08-09 08:28:15 +0300

    Some routing

--- project.clj
+++ project.clj
@@ -2,15 +2,16 @@
   :description "Grocery api"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [environ "1.1.0"]
-                 [ring/ring-core "1.6.1"]
-                 [ring/ring-jetty-adapter "1.6.1"]
+                 [migratus "0.9.5"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1206-jdbc42"]
-                 [migratus "0.9.5"]]
+                 [ring/ring-core "1.6.1"]
+                 [ring/ring-jetty-adapter "1.6.1"]
+                 [compojure "1.6.0"]]
   :plugins [[lein-ring "0.12.0"]
             [lein-environ "1.1.0"]
             [migratus-lein "0.4.9"]
             [lein-pprint "1.1.2"]]
-  :ring {:handler groceryapi.core/handler}
+  :ring {:handler groceryapi.core/app}
   :migratus {:store :database
              :migration-dir "migrations"})
--- src/groceryapi/core.clj
+++ src/groceryapi/core.clj
@@ -1,7 +1,14 @@
 (ns groceryapi.core
-  (:require [groceryapi.db :as catalog]))
+  (:require [groceryapi.db :as catalog]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/json"}
-   :body (clojure.string/join "\n" (catalog/catalog))})
+;(defn handler [request]
+;  {:status 200
+;   :headers {"Content-Type" "text/json"}
+;   :body (clojure.string/join "\n" (catalog/catalog))})
+
+(defroutes app
+           (GET "/" [] "Hello")
+           (GET "/catalog" [] (clojure.string/join "\n" (catalog/catalog)))
+           (route/not-found "Not found"))
